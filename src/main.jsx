f43d33bd6776c7a21d188058d04caac179f2f050
biswajitbesra <biswajitbesra28@gmail.com>
2024-12-24 12:40:30 +0530

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,7 @@ const router = createBrowserRouter(
         </Route>
     ),
     {
-        basename: "/personal-portfolio/"
+        basename: "/portfolio-website/"
     }
 )
 
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/personal-portfolio/',
-    build: {
-        outDir: 'dist',
-    },
+    base: '/portfolio-website/',
     plugins: [react()],
 })
